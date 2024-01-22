68fee4eb576b1890776949c445d1c3fd2950cb1e
Bryan Vera <brayanandresveragar@gmail.com>
2024-01-22 09:43:53 -0500

    Estilización

--- force-app/main/default/lwc/movieList/movieList.js
+++ force-app/main/default/lwc/movieList/movieList.js
@@ -6,28 +6,6 @@ export default class MovieList extends LightningElement {
     @wire(getAllMovies)
     movies;
 
-    // movies.data --> datos devueltos por apex
-    // movies.error --> Si error se almacena aqui
-
-
-    // movies = [
-    //     {
-    //         Name : "Max Steel",
-    //         Description : "Esta es mi descripcion",
-    //         Image_Url: "https://play-lh.googleusercontent.com/FMfKk_6I6a3uPMKi03syTFs-PpSjsjNGbQN-oAoL3OJCpTs6MZBN4BLk4ax1ETZnOVj8",
-    //         Tags: ["Terror","Accion","Suspenso"]
-    //     },
-    //     {
-    //         Name : "Una casa embrujada",
-    //         Description : "Esta es mi descripcion",
-    //         Image_Url: "https://www.neoteo.com/wp-content/uploads/2008/03/casaembrujada.jpg",
-    //         Tags: ["Terror","Accion","Suspenso"]
-    //     },
-    //     {
-    //         Name : "El señor de los anillos",
-    //         Description : "Esta es mi descripcion",
-    //         Image_Url: "https://es.web.img3.acsta.net/pictures/22/10/04/16/26/1929878.jpg",
-    //         Tags: ["Terror","Accion","Suspenso"]
-    //     }
-    // ]
+    //movies.data -> Contains the data returned from the Apex method
+    //movies.error -> Contains the error returned from the Apex method
 }
