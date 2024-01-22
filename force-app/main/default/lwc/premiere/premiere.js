3d18a169d3e54326f03e3d965be02681cbd621dd
Bryan Vera <brayanandresveragar@gmail.com>
2024-01-22 11:47:25 -0500

    CACHE

--- force-app/main/default/lwc/premiere/premiere.js
+++ force-app/main/default/lwc/premiere/premiere.js
@@ -1,5 +1,21 @@
 import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from "lightning/navigation";
 
-export default class Premiere extends LightningElement {
+export default class Premiere extends NavigationMixin(LightningElement){
     @api movie;
+
+    get isGoodMovie(){
+        return this.movie.Rating__c.includes('Buena') || this.movie.Rating__c.includes('Muy Buena');
+    }
+
+    navigateToRecordViewPage() {
+      // View a custom object record.
+      this[NavigationMixin.Navigate]({
+        type: "standard__recordPage",
+        attributes: {
+          recordId: this.movie.Id,
+          actionName: "view",
+        },
+      });
+    }
 }
