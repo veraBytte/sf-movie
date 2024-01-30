9fc808b616420024442c94c64bca418b0f7235dd
Bryan Vera <brayanandresveragar@gmail.com>
2024-01-30 13:34:51 -0500

    Configuración scratch org

--- force-app/main/default/lwc/movieList/movieList.js
+++ force-app/main/default/lwc/movieList/movieList.js
@@ -4,12 +4,14 @@ import getFilterMovies from '@salesforce/apex/MovieListService.getFilterMovies';
 export default class MovieList extends LightningElement {
     searchText;
 
+    //Wire
     @wire(getFilterMovies,{ searchText: '$searchText'})
     movies;
 
     //movies.data -> Contains the data returned from the Apex method
     //movies.error -> Contains the error returned from the Apex method
 
+    //Methods
     handleInputChange(e){
         const searchKey = e.target.value;
         if(searchKey.length >= 2 || searchKey.length === 0){
